ae291c00673c89fb7a78e3723afb6a0d4bcd2297
Brasili <matheusbrasili.ifsp@gmail.com>
2018-04-15 12:07:11 -0300

    V1 - cabecalho.php

---
+++ cabecalho.php
@@ -0,0 +1,20 @@
+<header>
+
+	<link href="https://fonts.googleapis.com/css?family=Marcellus+SC&amp;subset=latin-ext" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+	<h1>Trabalho Prático</h1>
+	
+	<nav>
+	
+		<ul>
+		
+			<li><a href="index.php">Início</a></li>
+			<li><a href="form_estado.php">Cadastrar Estados</a></li>
+			<li><a href="form_cidade.php">Cadastrar Cidades</a></li>
+			<li><a href="form_cadastro.php">Cadastrar Pessoas</a></li>
+			
+		</ul>
+		
+	</nav>
+	
+</header>
